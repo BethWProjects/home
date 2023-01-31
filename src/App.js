6939d953dfd716c8c4fcc9ea2e131f8df354c448
Beth Wilson <bethwilson777@gmail.com>
2023-01-31 09:11:58 -0700

    Added state to search field and onClick functionality to redirect to the google search query results, interpolating the users input value into the search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import './index.css';
+import { useState } from 'react';
 
 function App() {
+
+  //set the useState array
+  const [userQuery, setUserQuery] = useState('')
+
+  //create a Search function to redirect a user to a new url upon search
+  //use the window.open() to redirect to a url specified
+  //interpolate the google url with a query assigned to the userQuery state value
+  //assign the userQuery value to the 'q' parameter 
+  //assing a second argument '_blank' telling the browser to open a new tab
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, '_blank')
+  }
+
+  //create a function to update the query and set state (this function will go within the onChange attribute, so event target is provided, use this event as an argument in function)
+  //event.target.value is the value the user enters into the field
+  const updateUserQuery = event => {
+
+    setUserQuery(event.target.value)
+  }
+
   return (
     <div className="App">
-      App
+      <h1>Hello Beth</h1>
+      <div className='form'>
+        <input value={userQuery} onChange={updateUserQuery}/>
+        <button onClick={searchQuery}>Search</button>
+      </div>
     </div>
   );
 }
