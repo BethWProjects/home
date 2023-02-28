2ff433b64201723163fcdd05ef94ba4bb51e18c6
Beth Wilson <bethwilson777@gmail.com>
2023-02-28 10:55:59 -0700

    Added delay useState for the user to dynamically enter a delay entry

--- src/MatrixDynamicInput.js
+++ src/MatrixDynamicInput.js
@@ -1,21 +1,42 @@
 import React, {useState, useEffect} from 'react';
 import MATRIX_FRAMES from './data/matrix';
 
+const MILLISECONDS = 100;
+const minimumDelay = 1 * MILLISECONDS
+
 function MatrixDynamicInput() {
     const [index, setIndex] = useState(0)
+    const [delay, setDelay] = useState(100)
 
     useEffect(() => {
-        setInterval(
+        console.log('delay', delay)
+        const interval = setInterval(
             () => {
              setIndex(storedIndex => {
                 return (storedIndex + 1) % MATRIX_FRAMES.length
             })
-            }, 100)
-    }, [])
+        }, delay);
 
+        return () => {
+            clearInterval(interval)
+        }
+    }, [delay])
+
+    const updateDelay = event => {
+        let delay = Number(event.target.value) * MILLISECONDS
+        setDelay(delay < minimumDelay ? minimumDelay : delay)
+    }
     return (
         <div className='Matrix'>Matrix
-            <img src={MATRIX_FRAMES[index]} alt="matrix-animation" /> 
+            <img src={MATRIX_FRAMES[index]} 
+            alt="matrix-animation" 
+            /> 
+            <div className=''>
+                <div>
+                    Matrix transition delay (seconds): 
+                    <input type="number" onChange={updateDelay} />
+                </div>
+            </div>
         </div>
     )
 }
